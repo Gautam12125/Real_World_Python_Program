77ebeef46f7b89308aa59215fca7520a27280cfd
Gautam Sharma <2019pietcsgautam63@poornima.org>
2020-11-27 21:11:40 +0530

    Update RemainderNotification.py

--- RemainderNotification.py
+++ RemainderNotification.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     while True:
         notification.notify(
         title = "**Please Drink Water Now!**",
-        message ='''You've probably heard the advice to drink eight glasses of water a day. 
-        That's easy to remember, and it's a reasonable goal.''',
+        message ='''You've probably heard the advice to drink eight glasses of water a day.That's easy to remember, and it's a reasonable goal.''',
         app_icon="icons/Iconsmind-Outline-Glass-Water.ico",
         timeout=2
         )
